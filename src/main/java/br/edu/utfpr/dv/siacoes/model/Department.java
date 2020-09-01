287221898f95b1c560a5f666058b5d5d1c47a83d
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-09-01 12:40:07 -0300

    implementa lombox na classe DepartmentMODEL

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
@@ -7,23 +7,12 @@ import lombok.Data;
 public class Department implements Serializable {
   private static final long serialVersionUID = 1L;
 
-  private int idDepartment;
-  private Campus campus;
-  private String name;
-  private String fullName;
-  private transient byte[] logo;
-  private boolean active;
-  private String site;
-  private String initials;
-
-  public Department() {
-    this.setIdDepartment(0);
-    this.setCampus(new Campus());
-    this.setName("");
-    this.setFullName("");
-    this.setLogo(null);
-    this.setActive(true);
-    this.setSite("");
-    this.setInitials("");
-  }
+  private int idDepartment = 0;
+  private Campus campus = new Campus();
+  private String name = "";
+  private String fullName = "";
+  private transient byte[] logo = null;
+  private boolean active = true;
+  private String site = "";
+  private String initials = "";
 }
